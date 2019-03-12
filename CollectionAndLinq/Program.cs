e44bb1cc43f150949400df2f85d64f43c7848618
Austin <austincumberlander@gmail.com>
2019-03-11 19:54:26 -0500

    Add project files.

---
+++ CollectionAndLinq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CollectionAndLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
